62f0ecabda0489f90714aeec5b35f66721260cb8
Alexandru Croitor <alexandru.croitor@wearepropeople.com>
2015-05-10 15:37:14 +0300

    Add union-find ranked by tree height.

--- unionfind.h
+++ unionfind.h
@@ -157,10 +157,71 @@ private:
     unsigned long size;
 };
 
+class WeightedQuickUnionUFHeight {
+public:
+    WeightedQuickUnionUFHeight (unsigned long n) : size(n) {
+        parents.reserve(n);
+        heights.reserve(n);
+        maxes.reserve(n);
+        for (unsigned long i = 0; i < n; i++) {
+            parents[i] = i;
+            heights[i] = 1;
+            maxes[i] = i;
+        }
+    }
+
+    void join(unsigned long a, unsigned long b) {
+        unsigned long a_component = root(a);
+        unsigned long b_component = root(b);
+        if (a_component == b_component) return;
+        if (heights[a_component] >= heights[b_component]) {
+            parents[b_component] = a_component;
+            if (heights[a_component] == heights[b_component]) heights[a_component]++;
+            maxes[a_component] = std::max(maxes[a_component], maxes[b_component]);
+        }
+        else {
+            parents[a_component] = b_component;
+            heights[b_component]++;
+            maxes[b_component] = std::max(maxes[a_component], maxes[b_component]);
+        }
+        size--;
+    }
+
+    unsigned long root(unsigned long a) {
+        while (a != parents[a]) {
+            parents[a] = parents[parents[a]];
+            a = parents[a];
+        }
+        return a;
+    }
+
+    bool connected(unsigned long a, unsigned long b) {
+        return root(a) == root(b);
+    }
+
+    unsigned long find(unsigned long a) {
+        return root(a);
+    }
+
+    unsigned long findComponentMax(unsigned long a) {
+        return maxes[root(a)];
+    }
+
+    unsigned long count() {
+        return size;
+    }
+
+private:
+    std::vector<unsigned long> parents;
+    std::vector<unsigned long> heights;
+    std::vector<unsigned long> maxes;
+    unsigned long size;
+};
+
 template <typename Impl>
 int testUFImpl() {
     std::fstream f;
-    f.open("data/tinyUF.txt", std::fstream::in);
+    f.open("data/mediumUF.txt", std::fstream::in);
     if ( (f.rdstate() & std::ifstream::failbit ) != 0 ) {
         std::cerr << "Error opening file.\n";
         return 1;
@@ -196,6 +257,8 @@ int testUF() {
     testUFImpl<QuickUnionUF>();
     std::cout << "Testing weighted quick union.\n";
     testUFImpl<WeightedQuickUnionUF>();
+    std::cout << "Testing weighted quick union ranked by height.\n";
+    testUFImpl<WeightedQuickUnionUFHeight>();
     return 0;
 }
 
